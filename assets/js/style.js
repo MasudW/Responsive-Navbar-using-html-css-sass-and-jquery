6b0edb9ad0d6aaf095fb7449ce13c8f317d77a74
Md Majidul Islam Masud <85607318+MasudW@users.noreply.github.com>
2022-04-21 18:29:24 +0600

    up to date

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
         $(this).toggleClass('fa-times');
         $('.site-nav-menu').toggleClass('mobile-menu-show');
     });
+    $('li a').click(function () { 
+        $('.mobile-nav i').removeClass('fa-times');
+        $('.site-nav-menu').removeClass('mobile-menu-show');
+    });
     // $('.mobile-nav i').click(function () { 
     //     $('.site-nav-menu').toggleClass('mobile-menu-show')
     // });
